9dc87d67dc2039af9f6654ed38761506bc8ef95b
TSS@mail.tmysam.top <TSS@mail.tmysam.top>
2025-09-17 14:52:43 +0800

    Add slab

--- kernel/spinlock.h
+++ kernel/spinlock.h
@@ -1,9 +1,12 @@
 // Mutual exclusion lock.
+#ifndef SPINLOCK_H
+#define SPINLOCK_H
 struct spinlock {
-  uint locked;       // Is the lock held?
+    uint locked;  // Is the lock held?
 
-  // For debugging:
-  char *name;        // Name of lock.
-  struct cpu *cpu;   // The cpu holding the lock.
+    // For debugging:
+    char *name;       // Name of lock.
+    struct cpu *cpu;  // The cpu holding the lock.
 };
 
+#endif
