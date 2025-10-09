db7dd74b5fec56816bfb0a7ad6bd5b3a21ba0a19
TSS@mail.tmysam.top <TSS@mail.tmysam.top>
2025-10-09 21:10:22 +0800

    add allocate superpage

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -10,73 +10,106 @@
 #include "defs.h"
 
 void freerange(void *pa_start, void *pa_end);
+void superfreerange(void *pa_start, void *pa_end);
 
-extern char end[]; // first address after kernel.
-                   // defined by kernel.ld.
+extern char end[];  // first address after kernel.
+                    // defined by kernel.ld.
 
 struct run {
-  struct run *next;
+    struct run *next;
 };
 
 struct {
-  struct spinlock lock;
-  struct run *freelist;
+    struct spinlock lock;
+    struct run *freelist;
+    struct run *superlist;
 } kmem;
 
-void
-kinit()
-{
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+void kinit() {
+    initlock(&kmem.lock, "kmem");
+    freerange(end, (void *)PHYSTOP);
+    superfreerange((void *)PHYSTOP, (void *)SUPERSTOP);
 }
 
-void
-freerange(void *pa_start, void *pa_end)
-{
-  char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+void freerange(void *pa_start, void *pa_end) {
+    char *p;
+    p = (char *)PGROUNDUP((uint64)pa_start);
+    for (; p + PGSIZE <= (char *)pa_end; p += PGSIZE)
+        kfree(p);
+}
+
+void superfreerange(void *pa_start, void *pa_end) {
+    char *p;
+    p = (char *)SUPERPGROUNDUP((uint64)pa_start);
+    for (; p + SUPERPGSIZE <= (char *)pa_end; p += SUPERPGSIZE) {
+        superfree(p);
+    }
 }
 
 // Free the page of physical memory pointed at by pa,
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
-void
-kfree(void *pa)
-{
-  struct run *r;
+void kfree(void *pa) {
+    struct run *r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
-    panic("kfree");
+    if (((uint64)pa % PGSIZE) != 0 || (char *)pa < end || (uint64)pa >= PHYSTOP)
+        panic("kfree");
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+    // Fill with junk to catch dangling refs.
+    memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+    r = (struct run *)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+    acquire(&kmem.lock);
+    r->next = kmem.freelist;
+    kmem.freelist = r;
+    release(&kmem.lock);
+}
+
+void superfree(void *pa) {
+    struct run *r;
+
+    if (((uint64)pa % SUPERPGSIZE) != 0 || (char *)pa < (char *)PHYSTOP || (uint64)pa >= SUPERSTOP)
+        panic("superfree");
+    // Fill with junk to catch dangling refs.
+    memset(pa, 1, SUPERPGSIZE);
+    r = (struct run *)pa;
+    acquire(&kmem.lock);
+    r->next = kmem.superlist;
+    kmem.superlist = r;
+    release(&kmem.lock);
 }
 
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
 void *
-kalloc(void)
-{
-  struct run *r;
+kalloc(void) {
+    struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    acquire(&kmem.lock);
+    r = kmem.freelist;
+    if (r)
+        kmem.freelist = r->next;
+    release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+    if (r)
+        memset((char *)r, 5, PGSIZE);  // fill with junk
+    return (void *)r;
+}
+
+void *
+superalloc(void) {
+    struct run *r;
+
+    acquire(&kmem.lock);
+    r = kmem.superlist;
+    if (r)
+        kmem.superlist = r->next;
+    release(&kmem.lock);
+
+    if (r)
+        memset((char *)r, 5, SUPERPGSIZE);  // fill with junk
+    return (void *)r;
 }
--- kernel/proc.c
+++ kernel/proc.c
@@ -203,7 +203,7 @@ proc_pagetable(struct proc *p) {
 
     // map the usyscall page, for system calls.
     if (mappages(pagetable, USYSCALL, PGSIZE,
-                 (uint64)(p->usyscall), PTE_R | PTE_U | PTE_X) < 0) {
+                 (uint64)(p->usyscall), PTE_R | PTE_U) < 0) {
         uvmunmap(pagetable, TRAMPOLINE, 1, 0);
         uvmunmap(pagetable, TRAPFRAME, 1, 0);
         uvmfree(pagetable, 0);
