2c5c1fd70d7a57cea150c63c677bf54dbad64b94
TSS@mail.tmysam.top <TSS@mail.tmysam.top>
2025-11-15 09:45:42 +0800

    add kthread test (disabled)

--- kernel/defs.h
+++ kernel/defs.h
@@ -105,6 +105,7 @@ int either_copyout(int user_dst, uint64 dst, void* src, uint64 len);
 int either_copyin(void* dst, int user_src, uint64 src, uint64 len);
 void procdump(void);
 struct proc* kthread_create(void (*func)(void*), void* arg, char* name);
+void kthread_test_init(void);
 
 // swtch.S
 void swtch(struct context*, struct context*);
